7edff78be9d769731d3432a902e4657a0d85e70e
nick.a.sarbicki@gmail.com <Nick.A.Sarbicki@gmail.com>
2016-05-02 06:34:32 +0100

    Adding rest_framework to restaurant manager

--- restaurant_manager/restaurant_manager/example_settings.py
+++ restaurant_manager/restaurant_manager/example_settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookings.apps.BookingsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE_CLASSES = [
