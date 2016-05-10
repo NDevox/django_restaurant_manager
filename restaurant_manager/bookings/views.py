fdc4b29f7c8742c40c5e1eb9894a968752d20b2e
nick.a.sarbicki@gmail.com <Nick.A.Sarbicki@gmail.com>
2016-05-10 11:06:44 +0100

    Setting up a basic api view.

--- restaurant_manager/bookings/views.py
+++ restaurant_manager/bookings/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from bookings.models import Restaurant, Booking
+from rest_framework.renderers import JSONRenderer
+
 from bookings.forms import BookingForm, RestaurantForm, RestaurantChoiceForm
+from bookings.models import Restaurant, Booking
+from bookings.serializers import RestaurantSerializer
+
+
+class JSONResponse(HttpResponse):
+    """
+    HttpResponse which returns JSON.
+    """
+    def __init__(self, data, *args, **kwargs):
+        """
+        Add JSON Data to HTTPResponse init
+
+        :param data: JSON Data to add.
+        """
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, *args, **kwargs)
+
+
+def get_restaurants(request):
+    """
+    List all Restaurants.
+
+    :param request:
+    :return: JSONResponse, JSON of all the Restaurants.
+    """
+
+    if request.method == 'GET':
+        restaurants = Restaurant.objects.all()
+        serializer = RestaurantSerializer(restaurants, many=True)
+        return JSONResponse(serializer.data)
+
 
 
 def make_restaurant(request):
